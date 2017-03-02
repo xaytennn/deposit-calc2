a32de0a0a1b433b98dae7afe1b1aa502a1fd5158
Renat Gaynulin <Renat Gaynulin>
2017-03-02 20:21:22 +0700

    Creating a function of calculating the income from the deposit

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int checkS(int summ);
 int checkD(int date);
+int check(int summ, int date);
 
 int main()
 {
@@ -25,6 +26,8 @@ int main()
         }
     } while (i == -1);
 
+    summ = check(summ, date);
+    printf("Your deposit amount is %d/\n", summ);
     return 0;
 }
 
@@ -44,4 +47,31 @@ int checkD(int date)
         i = 1;
     }
     return i;
+}
+
+int check(int summ, int date)
+{
+    int profit;
+    if (summ <= 100000) {
+        if (date <= 30) {
+            profit = summ * 9 / 10;
+        } else if (date <= 120) {
+            profit = summ * 102 / 100;
+        } else if (date <= 240) {
+            profit = summ * 106 / 100;
+        } else {
+            profit = summ * 112 / 100;
+        }
+    } else {
+        if (days <= 30) {
+            profit = summ * 9 / 10;
+        } else if (date <= 120) {
+            profit = summ * 103 / 100;
+        } else if (date <= 240) {
+            profit = summ * 108 / 100;
+        } else {
+            profit = summ * 115 / 100;
+        }
+    }
+    return profit;
 }
